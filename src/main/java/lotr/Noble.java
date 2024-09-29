0f084a3a6987b9794b67a88d0b405c7b5b7643bd
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-09-29 18:47:31 +0300

    Rewrote random for abstract class Noble to work for older versions of java

--- src/main/java/lotr/Noble.java
+++ src/main/java/lotr/Noble.java
@@ -5,6 +5,6 @@ import lotr.kickstategy.NobleKick;
 
 public abstract class Noble extends Character{
     public Noble(int min, int max){
-        super(new Random().nextInt(min, max), new Random().nextInt(min, max), new NobleKick());
+        super(new Random().nextInt(max - min) + min, new Random().nextInt(max - min) + min, new NobleKick());
     }
 }
