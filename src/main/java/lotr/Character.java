bf160621ed06c62cb1af018e7bb132d543956ae8
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-09-29 19:03:41 +0300

    Activated tests and rewrote setter for hp in abstract class Character

--- src/main/java/lotr/Character.java
+++ src/main/java/lotr/Character.java
@@ -5,11 +5,20 @@ import lombok.Getter;
 import lombok.Setter;
 import lotr.kickstategy.KickStrategy;
 
-@Setter @Getter @AllArgsConstructor
+@Getter @AllArgsConstructor
 public abstract class Character {
     private int hp;
+    @Setter
     private int power;
     private KickStrategy kickStrategy;
+
+    public void setHp(int hp) {
+        if (hp < 0) {
+            this.hp = 0;
+        } else {
+            this.hp = hp;
+        }
+    }
 
     public void kick(Character c){
         kickStrategy.kick(this, c);
--- src/test/java/KingTest.java
+++ src/test/java/KingTest.java
@@ -1,26 +1,29 @@
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-// import static org.junit.jupiter.api.Assertions.assertTrue;
+import lotr.King;
+import lotr.Character;
 
-// public class KingTest {
-//     private Character character;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-//     @BeforeEach
-//     public void init() { character = new King(); }
+public class KingTest {
+    private Character character;
 
-//     @Test
-//     public void testIsClass() {
-//         assertTrue(character instanceof Character);
-//     }
+    @BeforeEach
+    public void init() { character = new King(); }
 
-//     @Test
-//     public void testHp() {
-//         assertTrue(character.getHp() >= 5 && character.getHp() <= 15);
-//     }
+    @Test
+    public void testIsClass() {
+        assertTrue(character instanceof Character);
+    }
 
-//     @Test
-//     public void testPower() {
-//         assertTrue(character.getPower() >= 5 && character.getPower() <= 15);
-//     }
-// }
+    @Test
+    public void testHp() {
+        assertTrue(character.getHp() >= 5 && character.getHp() <= 15);
+    }
+
+    @Test
+    public void testPower() {
+        assertTrue(character.getPower() >= 5 && character.getPower() <= 15);
+    }
+}
