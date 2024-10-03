6cf1b6cb691a31fe9f92d04f01e49df5c5cd10c2
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-10-03 13:14:04 +0300

    Implemented CharacterFactory using reflections and added reflections to pom.xml

--- src/main/java/lotr/CharacterFactory.java
+++ src/main/java/lotr/CharacterFactory.java
@@ -1,10 +1,32 @@
 package lotr;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.Set;
+
+import org.reflections.Reflections;
+
 
 public class CharacterFactory {
-    // public Character createCharacter(){
-    //     Class<? extends Character> characters[] = {Hobbit.class};
-    //     return characters[new Random().nextInt(characters.length)].;
-    // }
+    private List<Class<? extends Character>> nonAbstractSubTypes = new ArrayList<>();
+
+    public CharacterFactory() {
+        Reflections reflections = new Reflections("lotr");
+        Set<Class<? extends Character>> subTypes = reflections.getSubTypesOf(Character.class);
+        for (Class<? extends Character> el: subTypes) {
+            if (!Modifier.isAbstract(el.getModifiers())) {
+                nonAbstractSubTypes.add(el);
+            }
+        }
+    }
+
+    public Character createCharacter() throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException{
+        Random random = new Random();
+        Class<? extends Character> randomClass = nonAbstractSubTypes.get(random.nextInt(nonAbstractSubTypes.size()));
+        Character randomInstance = randomClass.getDeclaredConstructor().newInstance();
+        return randomInstance;
+    }
 }
